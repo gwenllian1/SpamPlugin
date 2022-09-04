f96955b7e201775ebb5ae5b8d4710ba8d09ee53d
gwenllian1 <llio.vernon@gmail.com>
2022-09-04 23:35:26 +1000

    first implementation of allowing multiple answers

--- functions/lib/spamAnswers.js
+++ functions/lib/spamAnswers.js
@@ -7,6 +7,84 @@ const db_1 = require("./db");
 //const { snapshot } = require("firebase-functions");
 //const admin = require("firebase-admin");
 const db = admin.database();
+const nextRound = async (meetingId, timestamp) => {
+    // move to next round
+    let { currentSection } = (await db.ref(`/config/${meetingId}/current`).get()).val();
+    if (typeof currentSection == "string") {
+        currentSection = parseInt(currentSection);
+    }
+    await db_1.scheduleFirebaseUpdate(meetingId, timestamp, `config/${meetingId}/current/currentSection`, parseInt(currentSection) + 1);
+};
+const updateDatabase = async (meetingId, roundName, msgSender, msgSenderName, messageContent, timestamp, teamId) => {
+    // store answer in db
+    await db_1.setValInDb(`data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`, {
+        senderId: msgSender,
+        senderName: msgSenderName,
+        answer: messageContent,
+        timestamp,
+        teamId,
+    });
+    // update score for the team
+    var questionWeight = (await db
+        .ref(`/config/${meetingId}/current/currentState/plugins/spammessages/questionWeight`)
+        .get()).val();
+    await db_1.incrementTeamsScore(meetingId, teamId, questionWeight, timestamp);
+};
+const correctAnswerMultiple = async (meetingId, teamId, roundName, messageContent, timestamp, msgSender, msgSenderName) => {
+    const teamBotId = await db_1.getBotForTeam(meetingId, teamId);
+    if (!teamBotId)
+        return;
+    //checking if the answer has been submitted before
+    var previousAnswers = (await db
+        .ref(`data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`)
+        .get()).val();
+    console.log("previous answers");
+    console.log(previousAnswers);
+    //the answer is in the db, check if the team submitted it
+    if (previousAnswers) {
+        const answered = Object.values(previousAnswers).find((val) => val === messageContent.toLowerCase().trim()) !== undefined;
+        console.log("answered");
+        console.log(answered);
+        if (!answered) {
+            //if the team has not submitted the answer before, update the database otherwise inform them
+            //that the answer is not valid.
+            await db
+                .ref(`data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`)
+                .push({
+                senderId: msgSender,
+                senderName: msgSenderName,
+                answer: messageContent,
+                timestamp,
+                teamId,
+            });
+            await db_1.broadcastMessage(meetingId, teamBotId, `Well done! ${messageContent} was a correct answer`);
+        }
+        else {
+            await db_1.broadcastMessage(meetingId, teamBotId, `${messageContent} has already been submitted, please try a different answer`);
+        }
+    }
+    else {
+        await updateDatabase(meetingId, roundName, msgSender, msgSenderName, messageContent, timestamp, teamId);
+        await db_1.broadcastMessage(meetingId, teamBotId, `Well done! ${messageContent} was a correct answer`);
+    }
+};
+const correctAnswerSingle = async (meetingId, teamId, messageContent, timestamp) => {
+    // send chat to this team to tell them they got the correct answer
+    const teamBotId = await db_1.getBotForTeam(meetingId, teamId);
+    if (!teamBotId)
+        return;
+    await db_1.broadcastMessage(meetingId, teamBotId, `Well done! ${messageContent} was the correct answer, we are moving on to the next round`);
+    // send message to all other teams telling them the correct answer
+    const botIds = await db_1.getAllBots(meetingId);
+    botIds.forEach(async (id) => {
+        // if is winning team's bot ignore
+        if (id === teamBotId) {
+            return;
+        }
+        await db_1.broadcastMessage(meetingId, id, `Unfortunately, Team ${teamId} guessed the correct answer (${messageContent}), better luck next time!`);
+    });
+    await nextRound(meetingId, timestamp);
+};
 exports.spamAnswers = functions.database
     .ref("/data/chats/{meetingId}/{sensor}/{chatId}")
     .onCreate(async (value, context) => {
@@ -35,40 +113,21 @@ exports.spamAnswers = functions.database
         const teamId = await db_1.getPlayersTeam(msgSender.toString(), meetingId);
         if (!teamId)
             return;
-        // store answer in db
         const roundName = await db_1.getValFromDb(`/config/${meetingId}/current/currentState/plugins/spammessages/roundName`);
-        await db_1.setValInDb(`data/spamAnswers/${meetingId}/${roundName}/answers`, {
-            senderId: msgSender,
-            senderName: msgSenderName,
-            answer: messageContent,
-            timestamp,
-            teamId,
-        });
-        // update score for the team
-        var questionWeight = (await db
-            .ref(`/config/${meetingId}/current/currentState/plugins/spammessages/questionWeight`)
-            .get()).val();
-        await db_1.incrementTeamsScore(meetingId, teamId, questionWeight, timestamp);
-        // send chat to this team to tell them they got the correct answer
         const teamBotId = await db_1.getBotForTeam(meetingId, teamId);
         if (!teamBotId)
             return;
-        await db_1.broadcastMessage(meetingId, teamBotId, `Well done! ${messageContent} was the correct answer, we are moving on to the next round`);
-        // send message to all other teams telling them the correct answer
-        const botIds = await db_1.getAllBots(meetingId);
-        botIds.forEach(async (id) => {
-            // if is winning team's bot ignore
-            if (id === teamBotId) {
-                return;
-            }
-            await db_1.broadcastMessage(meetingId, id, `Unfortunately, Team ${teamId} guessed the correct answer (${messageContent}), better luck next time!`);
-        });
-        // move to next round
-        let { currentSection } = (await db.ref(`/config/${meetingId}/current`).get()).val();
-        if (typeof currentSection == "string") {
-            currentSection = parseInt(currentSection);
+        const roundType = await db_1.getValFromDb(`/config/${meetingId}/current/currentState/plugins/spammessages/roundType`);
+        console.log(roundType);
+        if (typeof roundType === "string" &&
+            roundType.toLowerCase().trim() === "single_answer") {
+            await updateDatabase(meetingId, roundName, msgSender, msgSenderName, messageContent, timestamp, teamId);
+            await correctAnswerSingle(meetingId, teamId, messageContent, timestamp);
         }
-        await db_1.scheduleFirebaseUpdate(meetingId, timestamp, `config/${meetingId}/current/currentSection`, parseInt(currentSection) + 1);
+        else if (typeof roundType === "string" &&
+            roundType.toLowerCase().trim() === "multiple_answer") {
+            await correctAnswerMultiple(meetingId, teamId, roundName, messageContent, timestamp, msgSender, msgSenderName);
+        }
     }
 });
 //# sourceMappingURL=spamAnswers.js.map
--- functions/src/spamAnswers.ts
+++ functions/src/spamAnswers.ts
@@ -14,6 +14,164 @@ import {
 //const { snapshot } = require("firebase-functions");
 //const admin = require("firebase-admin");
 const db = admin.database();
+
+const nextRound = async (meetingId: string, timestamp: any) => {
+  // move to next round
+  let { currentSection } = (
+    await db.ref(`/config/${meetingId}/current`).get()
+  ).val();
+  if (typeof currentSection == "string") {
+    currentSection = parseInt(currentSection);
+  }
+
+  await scheduleFirebaseUpdate(
+    meetingId,
+    timestamp,
+    `config/${meetingId}/current/currentSection`,
+    parseInt(currentSection) + 1
+  );
+};
+
+const updateDatabase = async (
+  meetingId: string,
+  roundName: unknown,
+  msgSender: string,
+  msgSenderName: string,
+  messageContent: string,
+  timestamp: any,
+  teamId: string
+) => {
+  // store answer in db
+  await setValInDb(
+    `data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`,
+    {
+      senderId: msgSender,
+      senderName: msgSenderName,
+      answer: messageContent,
+      timestamp,
+      teamId,
+    }
+  );
+
+  // update score for the team
+  var questionWeight: number = (
+    await db
+      .ref(
+        `/config/${meetingId}/current/currentState/plugins/spammessages/questionWeight`
+      )
+      .get()
+  ).val();
+  await incrementTeamsScore(meetingId, teamId, questionWeight, timestamp);
+};
+
+const correctAnswerMultiple = async (
+  meetingId: string,
+  teamId: string,
+  roundName: unknown,
+  messageContent: string,
+  timestamp: any,
+  msgSender: string,
+  msgSenderName: string
+) => {
+  const teamBotId = await getBotForTeam(meetingId, teamId);
+  if (!teamBotId) return;
+  //checking if the answer has been submitted before
+  var previousAnswers: string[] = (
+    await db
+      .ref(
+        `data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`
+      )
+      .get()
+  ).val();
+  console.log("previous answers");
+  console.log(previousAnswers);
+  //the answer is in the db, check if the team submitted it
+  if (previousAnswers) {
+    const answered =
+      Object.values(previousAnswers).find(
+        (val) => val === messageContent.toLowerCase().trim()
+      ) !== undefined;
+    console.log("answered");
+    console.log(answered);
+    if (!answered) {
+      //if the team has not submitted the answer before, update the database otherwise inform them
+      //that the answer is not valid.
+      await db
+        .ref(
+          `data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`
+        )
+        .push({
+          senderId: msgSender,
+          senderName: msgSenderName,
+          answer: messageContent,
+          timestamp,
+          teamId,
+        });
+      await broadcastMessage(
+        meetingId,
+        teamBotId,
+        `Well done! ${messageContent} was a correct answer`
+      );
+    } else {
+      await broadcastMessage(
+        meetingId,
+        teamBotId,
+        `${messageContent} has already been submitted, please try a different answer`
+      );
+    }
+  } else {
+    await db
+      .ref(
+        `data/plugins/spamAnswers/${meetingId}/${roundName}/answers/${teamId}`
+      )
+      .push({
+        senderId: msgSender,
+        senderName: msgSenderName,
+        answer: messageContent,
+        timestamp,
+        teamId,
+      });
+    await broadcastMessage(
+      meetingId,
+      teamBotId,
+      `Well done! ${messageContent} was a correct answer`
+    );
+  }
+};
+
+const correctAnswerSingle = async (
+  meetingId: string,
+  teamId: string,
+  messageContent: string,
+  timestamp: any
+) => {
+  // send chat to this team to tell them they got the correct answer
+  const teamBotId = await getBotForTeam(meetingId, teamId);
+  if (!teamBotId) return;
+
+  await broadcastMessage(
+    meetingId,
+    teamBotId,
+    `Well done! ${messageContent} was the correct answer, we are moving on to the next round`
+  );
+
+  // send message to all other teams telling them the correct answer
+  const botIds = await getAllBots(meetingId);
+  botIds.forEach(async (id) => {
+    // if is winning team's bot ignore
+    if (id === teamBotId) {
+      return;
+    }
+    await broadcastMessage(
+      meetingId,
+      id,
+      `Unfortunately, Team ${teamId} guessed the correct answer (${messageContent}), better luck next time!`
+    );
+  });
+
+  await nextRound(meetingId, timestamp);
+};
+
 export const spamAnswers = functions.database
   .ref("/data/chats/{meetingId}/{sensor}/{chatId}")
   .onCreate(async (value, context) => {
@@ -56,66 +214,43 @@ export const spamAnswers = functions.database
     if (answerCorrect) {
       const teamId = await getPlayersTeam(msgSender.toString(), meetingId);
       if (!teamId) return;
-
-      // store answer in db
       const roundName = await getValFromDb(
         `/config/${meetingId}/current/currentState/plugins/spammessages/roundName`
       );
-      await setValInDb(`data/spamAnswers/${meetingId}/${roundName}/answers`, {
-        senderId: msgSender,
-        senderName: msgSenderName,
-        answer: messageContent,
-        timestamp,
-        teamId,
-      });
-
-      // update score for the team
-      var questionWeight: number = (
-        await db
-          .ref(
-            `/config/${meetingId}/current/currentState/plugins/spammessages/questionWeight`
-          )
-          .get()
-      ).val();
-      await incrementTeamsScore(meetingId, teamId, questionWeight, timestamp);
-
-      // send chat to this team to tell them they got the correct answer
       const teamBotId = await getBotForTeam(meetingId, teamId);
       if (!teamBotId) return;
-
-      await broadcastMessage(
-        meetingId,
-        teamBotId,
-        `Well done! ${messageContent} was the correct answer, we are moving on to the next round`
+      const roundType = await getValFromDb(
+        `/config/${meetingId}/current/currentState/plugins/spammessages/roundType`
       );
 
-      // send message to all other teams telling them the correct answer
-      const botIds = await getAllBots(meetingId);
-      botIds.forEach(async (id) => {
-        // if is winning team's bot ignore
-        if (id === teamBotId) {
-          return;
-        }
-        await broadcastMessage(
+      console.log(roundType);
+      if (
+        typeof roundType === "string" &&
+        roundType.toLowerCase().trim() === "single_answer"
+      ) {
+        await updateDatabase(
           meetingId,
-          id,
-          `Unfortunately, Team ${teamId} guessed the correct answer (${messageContent}), better luck next time!`
+          roundName,
+          msgSender,
+          msgSenderName,
+          messageContent,
+          timestamp,
+          teamId
         );
-      });
-
-      // move to next round
-      let { currentSection } = (
-        await db.ref(`/config/${meetingId}/current`).get()
-      ).val();
-      if (typeof currentSection == "string") {
-        currentSection = parseInt(currentSection);
+        await correctAnswerSingle(meetingId, teamId, messageContent, timestamp);
+      } else if (
+        typeof roundType === "string" &&
+        roundType.toLowerCase().trim() === "multiple_answer"
+      ) {
+        await correctAnswerMultiple(
+          meetingId,
+          teamId,
+          roundName,
+          messageContent,
+          timestamp,
+          msgSender,
+          msgSenderName
+        );
       }
-
-      await scheduleFirebaseUpdate(
-        meetingId,
-        timestamp,
-        `config/${meetingId}/current/currentSection`,
-        parseInt(currentSection) + 1
-      );
     }
   });
